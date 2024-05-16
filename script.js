28484b52a97c9ddd28a238099ab9078dd749f19e
Karin <k.brotjefors@gmail.com>
2024-05-16 16:01:56 +0200

    test

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 function sendTestNotification() {
 
-    var url = 'https://http://brotjefors.pythonanywhere.com/send-notification';
-  
+    var url = 'http://brotjefors.pythonanywhere.com/send-notification';
+
     // The data to send to the server-side script
     var data = {
-      title: 'Test Notification',
-      body: 'This is a test notification from the WebView.',
+        title: 'Test Notification',
+        body: 'This is a test notification from the WebView.',
     };
-  
+
     // Send the data to the server-side script
     fetch(url, {
-      method: 'POST',
-      headers: {
+        method: 'POST',
+        headers: {
         'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
+        },
+        body: JSON.stringify(data),
     });
-  }
+}
+
+function test() {
+    var url = 'http://brotjefors.pythonanywhere.com/test';
+
+    fetch(url, { method: 'GET' })
+    .then(response => response.text())
+    .then(data => console.log(data))
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
