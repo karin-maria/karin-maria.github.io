1503893de8da4317df0800354794176732899d89
Karin <k.brotjefors@gmail.com>
2024-05-17 14:20:39 +0200

    test

--- server.py
+++ server.py
@@ -6,54 +6,44 @@ from firebase_admin import credentials, messaging
 
 app = Flask(__name__)
 
-print('FIREBASE_SERVICE_ACCOUNT:', os.getenv('FIREBASE_SERVICE_ACCOUNT'))
-
+# Initialize Firebase
 service_account_info = json.loads(os.getenv('FIREBASE_SERVICE_ACCOUNT'))
-
-print('Service Account Info:', service_account_info)
-
 cred = credentials.Certificate(service_account_info)
-
-print('Credential:', cred)
-
 firebase_admin.initialize_app(cred)
 
-print('Firebase App Initialized!')
+registration_token = None
+
+@app.route('/receive-token', methods=['POST'])
+def receive_token():
+    jsondata = request.get_json()
+    registration_token = jsondata.get('token')
+
+    if registration_token is None:
+        return 'No token found in request', 400
+
+    # TODO: Store the token in your database
+
+    return 'Token received successfully', 200
 
 @app.route('/send-notification', methods=['POST'])
 def send_notification():
-    print('In send_notification!')
 
     jsondata = request.get_json()
 
-    print('JSON Data:', jsondata)
+    #'ddRvgtPeRoOO5pF8L5n5wk:APA91bF7P6vT1SyzpQi5tEf9gAXqfThhK9JIJBhneeEbNN5HdvN8LW-d-fC8je6b3bCfLnpDxt1WnuMCkj4RK6ehIma-4k1p0SPg_moTddq_rjPSddbHzfZ5ZTKBaCd0pnbbnyJtJv7d'
 
-    registration_token = 'ddRvgtPeRoOO5pF8L5n5wk:APA91bF7P6vT1SyzpQi5tEf9gAXqfThhK9JIJBhneeEbNN5HdvN8LW-d-fC8je6b3bCfLnpDxt1WnuMCkj4RK6ehIma-4k1p0SPg_moTddq_rjPSddbHzfZ5ZTKBaCd0pnbbnyJtJv7d'
-
-    #message = messaging.Message(
-    #    data={
-    #        'title': jsondata.get('title'),
-    #        'body': jsondata.get('body'),
-    #    },
-    #    token=registration_token,
-    #)
+    if registration_token is None:
+        return 'No token found in request', 400
 
     message = messaging.Message(
-    notification=messaging.Notification(
-        title=jsondata.get('title'),
-        body=jsondata.get('body'),
-    ),
-    token=registration_token,
-)
+        notification=messaging.Notification(
+            title=jsondata.get('title'),
+            body=jsondata.get('body'),
+        ),
+        token=registration_token,
+    )
 
-    print('Message:', message)
-
-    # Send a message to the device corresponding to the provided
-    # registration token.
     response = messaging.send(message)
-    # Response is a message ID string.
-    print('Successfully sent message:', response)
-
     return response
 
 @app.route('/test', methods=['GET'])
